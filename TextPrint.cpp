d31fbe5f3e7f1cf6ec75ac25c5fccd4df2d391d6
bslewis <bslewis@wisc.edu>
2011-12-08 15:59:18 -0800

    Updated some of the text stuff.

--- TextPrint.cpp
+++ TextPrint.cpp
@@ -13,8 +13,6 @@ TextPrint::~TextPrint(void)
 {
 }
 
-// Prints characters from GLUT in the 3d world.
-// TODO: figure out why the text is dissapearing though.
 /*
 GLUT_BITMAP_8_BY_13
 GLUT_BITMAP_9_BY_15
@@ -26,22 +24,65 @@ GLUT_BITMAP_HELVETICA_18
 
 */
 
+
+
+/*
+ * Prints characters from GLUT in the 3d world which will always face the screen.
+ *       Do not push or pop matrices before this!
+ */
+
+
 void TextPrint::bitText(int x, int y, int z, 
 	float r, float g, float b, void * font, char *string)
 {
-  glColor3f( r, g, b );
-  glRasterPos3f(x, y, z);
-  int len, i;
-  len = (int)strlen(string);
-  for (i = 0; i < len; i++) {
-    glutBitmapCharacter(font, string[i]);
-  }
+	glColor3f( r, g, b );
+	glRasterPos3f(x, y, z);
+	int len, i;
+	len = (int)strlen(string);
+	for (i = 0; i < len; i++) {
+		glutBitmapCharacter(font, string[i]);
+	}
 }
 
+// Standalone function that will automatically place words in a HUD.
 void TextPrint::hudText(int x, int y, int z, 
 	float r, float g, float b, void * font, char *string)
 {
-
+	glColor3f( r, g, b );
+	glRasterPos3f(x, y, z);
+	int len, i;
+	len = (int)strlen(string);
+	for (i = 0; i < len; i++) {
+		glutBitmapCharacter(font, string[i]);
+	}
 
 }
 
+/*	Stroke fonts:
+GLUT_STROKE_ROMAN
+GLUT_STROKE_MONO_ROMAN
+*/
+
+void TextPrint::strokePrint(int x, int y, int z, 
+	float r, float g, float b, void * font, char *string)
+{
+	glColor3f( r, g, b );
+	glRasterPos3f(x, y, z);
+	int len, i;
+	len = (int)strlen(string);
+	for (i = 0; i < len; i++) {
+		glutBitmapCharacter(font, string[i]);
+	}
+
+
+}
+void output(GLfloat x, GLfloat y, char *text)
+{
+    char *p;
+    
+    glPushMatrix();
+    glTranslatef(x, y, 0);
+    for (p = text; *p; p++)
+        glutStrokeCharacter(GLUT_STROKE_ROMAN, *p);
+    glPopMatrix();
+}
--- TextPrint.h
+++ TextPrint.h
@@ -3,15 +3,17 @@
 #ifndef H_TEXTPRINT
 #define H_TEXTPRINT
 
+#include "Model.h"
 
 class TextPrint
 {
 public:
 	TextPrint(void);
 	~TextPrint(void);
-	// This prints a 
 	void bitText(int x, int y, int z, float r, float g, float b, void * font, char *string);
 	void hudText(int x, int y, int z, float r, float g, float b, void * font, char *string);
+	void strokePrint(int x, int y, int z, float r, float g, float b, void * font, char *string);
+private:
 };
 
 #endif
